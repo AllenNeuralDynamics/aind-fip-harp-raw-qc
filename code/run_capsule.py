c0c0fa087fe6821621f3fa883bd23ada31a7d30a
arjunsridhar12345 <arjun.sridhar@alleninstitute.org>
2026-08-07 23:06:18 +0000

    feat: add structured logging

--- code/run_capsule.py
+++ code/run_capsule.py
@@ -1,12 +1,14 @@
 import json
 import logging
+import os
+
 from aind_physiology_fip import data_qc
+from log_schema import setup_logging
 from pathlib import Path
 from pydantic import Field
 from pydantic_settings import BaseSettings
 from qc_exporter import to_ads, QCCli
 
-logger = logging.getLogger(__name__)
 
 class InputSettings(BaseSettings, cli_parse_args=True):
     """
@@ -20,11 +22,10 @@ class InputSettings(BaseSettings, cli_parse_args=True):
         default=Path("/results/"), description="Output directory"
     )
 
-if __name__ == "__main__":
-    logging.basicConfig(
-        level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
-    )
-
+def run() -> None:
+    """
+    Entrypoint for executing
+    """
     settings = InputSettings()
     paths = tuple(settings.input_directory.glob("fib/*"))
     primary_data_path = [path for path in paths if path.is_dir()]
@@ -36,7 +37,24 @@ if __name__ == "__main__":
             "Multiple primary data assets attached. Only single asset needed"
         )
 
-    logger.info(f"Running qc on primary data at path {primary_data_path[0]}")
+    with open(settings.input_directory / "data_description.json", "r") as f:
+        data_description = json.load(f)
+    
+    acquisition_name = data_description["name"]
+    ### logging setup
+    process_name = os.getenv("PROCESS_NAME", "aind-fip-harp-qc-raw")
+    pipeline_name = os.getenv("PIPELINE_NAME", "")
+    setup_logging(
+        (Path(__file__).parent / "logging.yml").as_posix(),
+        model={
+            "acquisition_name": acquisition_name,
+            "process_name": process_name,
+            "pipeline_name": pipeline_name    
+        },
+    )
+
+    logging.info("Begin processing...", extra={"event_type": "stage_start"})
+    logging.info(f"Running qc on primary data {acquisition_name}")
     parsed_args = QCCli(
         data_path=primary_data_path[0], qc_json_path=settings.output_directory, asset_path=settings.output_directory / "qc-raw"
     )
@@ -48,4 +66,11 @@ if __name__ == "__main__":
         with open(parsed_args.qc_json_path / "quality_control.json", "w", encoding="utf-8") as f:
             f.write(qc_json.model_dump_json(indent=2))
     
-    logger.info(f"Finished qc. Output saved to {settings.output_directory}")
+    logging.info(f"Finished qc. Output saved to {settings.output_directory}")
+    logging.info("Pipeline stage completed", extra={"event_type": "stage_complete"})
+
+if __name__ == "__main__":
+    try:
+        run()
+    except Exception as e:
+        logging.exception("Pipeline stage failed", extra={"event_type": "stage_error"})
